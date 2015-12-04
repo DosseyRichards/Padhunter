80aaa9f81a2d7e99598ef16e1340a1929d22c265
DosseyRichards <Richardsdossey@gmail.com>
2015-12-03 21:35:38 -0500

    lots of progress. Next commit will be version one completed

--- routes/index.js
+++ routes/index.js
@@ -62,12 +62,157 @@ var Listing_Schema = new Schema({
     contact_email: String,
     housing_type: String,
     bedrooms: Number,
+    bathrooms: Number,
     city: String,
     neighborhood: String,
     img: Buffer,
     price: Number,
+    state: String
 
 });
+
+
+
+
+//////Mongoose models and a user model for later when user 
+var Listing = mongoose.model('Listing', Listing_Schema);
+
+var User_instance = mongoose.model('User', User_Schema);
+
+
+
+
+
+router.get('/test', function(req,res){
+  res.send(req.query.stuff);
+});
+
+
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: '', state_options:states});
+});
+
+///View for individual listings
+router.get('/listing-info/:listing_id', function(req, res){
+	Listing.findOne({_id:req.params.listing_id}, function(err, listing_details){
+////Concacting the image file name with the static file url I set up in app.js to get the url that file is stored under.
+///Then sending it to the view to be used as an image source.
+	var image_url = "/listing_images/" + listing_details.img;
+  var message_action = "/send-message/" + listing_details.id;
+  res.render('listing_details', {listing_info:listing_details, image_location:image_url, message_sender:message_action});
+	});
+});
+
+
+router.get('/all', function(req, res){
+  Listing.find({}, function(err, listing_details){
+    res.send(listing_details);
+  /*res.render('listing_details', {listing_info:listingimg.datails});*/
+  });
+});
+
+
+
+
+
+//search engine filter using the mongoose query builder. (check mongoose docs for more info and sytnax).
+///A little bit of callback hell here. Watchout!
+router.post('/search', function(req,res){
+  
+  console.log(req.body);
+  Listing.find().
+  where('price').gt(req.body.min).lt(req.body.max).
+  where('bedrooms').equals(req.body.bedrooms).
+  where('city').equals(req.body.city).
+  where('bathrooms').equals(req.body.bathrooms).
+  sort('bedrooms').
+  exec(function(err, listings_docs){
+    Listing.find().
+    where('city').equals(req.body.city).
+    where('price').gt(req.body.min).lt(req.body.max).
+    sort('price').
+    exec(function(err, listings_docs_city){
+
+
+
+          
+            if(err){console.log(err);}
+            if(!err){
+              var image_url_src = "/listing_images/"+listings_docs_city.img;
+              res.render('search', {Results:listings_docs, City_Results: listings_docs_city,image_url:image_url_src});
+            }
+
+    });
+  });
+});
+
+
+router.get('/search', function(req,res){
+  res.render('search', {Results:"",City_Results:""});
+});
+
+///testing the query for the search engine! 
+        router.get('/sort-price', function(req, res){
+          Listing.find({}).where('price').gt(1000).lt(8000).exec(function(err, listing_details){
+    res.send(listing_details);
+  /*res.render('listing_details', {listing_info:listingimg.datails});*/
+  });
+});
+
+/*Here I retrieve the id that is sent a URI parameter in the form from the listing details
+page. I then search the database for the document and then send the message to the documents email string.*/
+router.post('/send-message/:listing_id', function(req,res){
+  Listing.findOne({_id:req.params.listing_id}, function(err, listing_details){
+    var listing_email = listing_details.contact_email;
+    server.send({
+   text:    req.body.message, 
+   from:    "<padhuntertest@gmail.com>", 
+   to:      listing_email,
+   cc:      "else <else@your-email.com>",
+   subject: "Someone wants to know more about your listing"
+}, function(err, message) {
+    if(err){
+        console.log(err);
+    }
+    if(message){
+        res.render('index');
+
+        }
+    }
+
+    );
+  });
+});
+
+
+////View for adding a listing
+router.get('/add-listing', function(req,res){
+  res.render('add_listing',{state_options:states});
+});
+
+///Saving data from the from
+router.post('/add-listing',upload.single('image'), function(req,res){
+  var new_Listing = new Listing;
+  new_Listing.title = req.body.title;
+  new_Listing.city = req.body.city;
+  new_Listing.bedrooms = req.body.bedrooms;
+  new_Listing.bathrooms = req.body.bathrooms
+  new_Listing.state = req.body.state;
+  new_Listing.housing_type = req.body.housing_type;
+  new_Listing.description = req.body.description;
+  new_Listing.img = req.file.filename;
+  new_Listing.contact_email = req.body.email;
+  new_Listing.date_submitted = Date.now();
+  new_Listing.price = req.body.price;
+  new_Listing.save();;
+console.log(new_Listing);
+res.send('Success!');
+});
+
+
+
 
 
 var states = [
@@ -310,131 +455,6 @@ var states = [
 ];
 
 
-
-
-//////Mongoose models and a user model for later when user 
-var Listing = mongoose.model('Listing', Listing_Schema);
-
-var User_instance = mongoose.model('User', User_Schema);
-
-
-
-
-
-router.get('/test', function(req,res){
-  res.send(req.query.stuff);
-});
-
-
-
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: '', state_options:states});
-});
-
-///View for individual listings
-router.get('/listing-info/:listing_id', function(req, res){
-	Listing.findOne({_id:req.params.listing_id}, function(err, listing_details){
-////Concacting the image file name with the static file url I set up in app.js to get the url that file is stored under.
-///Then sending it to the view to be used as an image source.
-	var image_url = "/listing_images/" + listing_details.img;
-  var message_action = "/send-message/" + listing_details.id;
-  res.render('listing_details', {listing_info:listing_details, image_location:image_url, message_sender:message_action});
-	});
-});
-
-
-router.get('/all', function(req, res){
-  Listing.find({}, function(err, listing_details){
-    res.send(listing_details);
-  /*res.render('listing_details', {listing_info:listingimg.datails});*/
-  });
-});
-
-
-
-
-
-//search engine filter using the mongoose query builder. (check mongoose docs for more info and sytnax).
-///A little bit of callback hell here. Watchout!
-router.post('/search', function(req,res){
-  
-  console.log(req.body);
-  Listing.find().
-  where('price').gt(req.body.min).lt(req.body.max).
-  where('bedrooms').equals(req.body.bedrooms).
-  where('city').equals(req.body.city).
-  sort('bedrooms').
-  exec(function(err, listings_docs){
-    Listing.find().
-    where('city').equals(req.body.city).
-    where('price').gt(req.body.min).lt(req.body.max).
-    sort('bedrooms').
-    exec(function(err, listings_docs_city){
-
-
-
-          
-            if(err){console.log(err);}
-            if(!err){
-              var next_page_url ="" ;
-              res.render('search', {Results:listings_docs, City_Results: listings_docs_city});
-            }
-
-    });
-  });
-});
-
-
-router.get('/search', function(req,res){
-  res.render('search', {Results:"",City_Results:""});
-});
-
-///testing the query for the search engine! 
-        router.get('/sort-price', function(req, res){
-          Listing.find({}).where('price').gt(1000).lt(8000).exec(function(err, listing_details){
-    res.send(listing_details);
-  /*res.render('listing_details', {listing_info:listingimg.datails});*/
-  });
-});
-
-/*Here I retrieve the id that is sent a URI parameter in the form from the listing details
-page. I then search the database for the document and then send the message to the documents email string.*/
-router.post('/send-message/:listing_id', function(req,res){
-  Listing.findOne({_id:req.params.listing_id}, function(err, listing_details){
-    var listing_email = listing_details.contact_email;
-    server.send({
-   text:    req.body.message, 
-   from:    "<padhuntertest@gmail.com>", 
-   to:      listing_email,
-   cc:      "else <else@your-email.com>",
-   subject: "testing emailjs"
-}, function(err, message) { console.log(err || message); });
-  });
-});
-
-
-////View for adding a listing
-router.get('/add-listing', function(req,res){
-  res.render('add_listing');
-});
-
-///Saving data from the from
-router.post('/add-listing',upload.single('image'), function(req,res){
-  var new_Listing = new Listing;
-  new_Listing.title = req.body.title;
-  new_Listing.city = req.body.city;
-  new_Listing.bedrooms = req.body.bedrooms;
-  new_Listing.housing_type = req.body.housing_type;
-  new_Listing.description = req.body.description;
-  new_Listing.img = req.file.filename;
-  new_Listing.contact_email = req.body.email;
-  new_Listing.date_submitted = Date.now();
-  new_Listing.price = req.body.price;
-  new_Listing.save();
-  console.log(new_Listing.city);
-console.log(new_Listing);
-});
 
 module.exports = router;
 
